6479af2341305d2f347608796702048144d2b643
Jakub Kaderka <jakub.kaderka@gmail.com>
2014-05-25 15:15:00 +0200

    Switch to default layout fixed

--- widgets/kb_layout.lua
+++ widgets/kb_layout.lua
@@ -5,7 +5,7 @@ local wibox = require("wibox")
 kbdcfg = {}
 kbdcfg.cmd = "setxkbmap"
 kbdcfg.layout = { { "us", "" }, { "cz", "qwerty" }}
-kbdcfg.current = 2  -- default layout
+kbdcfg.current = 2 - 1  -- default layout - 1
 kbdcfg.widget = wibox.widget.textbox()
 kbdcfg.widget:set_text(" " .. kbdcfg.layout[kbdcfg.current][1] .. " ")
 
@@ -15,6 +15,9 @@ kbdcfg.switch = function ()
 	kbdcfg.widget:set_text(" " .. t[1] .. " ")
 	os.execute( kbdcfg.cmd .. " " .. t[1] .. " " .. t[2] )
 end
+
+-- switch to default layout
+kbdcfg.switch()
 
 -- Mouse bindings
 kbdcfg.widget:buttons(
